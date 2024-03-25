e505c414d5d070ea9d9d64f9918eccfcd2701ce0
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-03-25 12:16:11 +0600

    this is project name career hub and complete header and footer section in this project and it's developing

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,14 @@
+import Footer from '../../Layouts/Footer/Footer';
+import Header from '../../Layouts/Header/Header';
+
+const Root = () => {
+    return (
+        <div>
+          <Header></Header>
+            
+          <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Root from './components/Root/Root'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Root></Root>,
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
